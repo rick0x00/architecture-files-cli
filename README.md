b85c04f04a27e9c4ca5ea51e104b5a3539d9bc84
silvajhb <jhbs.student@gmail.com>
2021-09-19 20:19:45 -0300

    add link description use shell to README.md, and add source link project in code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # architecture files cli
+
+command execution
+
+'''shell
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/silvajhb/architecture-files-cli/master/mkarchdir.sh)"
+'''
--- mkarchdir.sh
+++ mkarchdir.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 localdir=$(pwd);
-
 cd "$localdir";
+
+GITHUBDIR(){
+	echo "Project source: https://github.com/silvajhb/architecture-files-cli";
+}
 
 print_usage(){
 	echo '';
+	GITHUBDIR;
 	echo 'Usage: mkarchdir.sh1 [ -h ] [ -s ] [ -d ] [ -R ]';
 	echo 'Option: (optional)';
 	echo '	-h	Add Hardware Directories';
@@ -14,3 +18,23 @@ print_usage(){
 	echo '	-R	Add README.md Files in all Directories(All README.md files recept respective title)';
 	echo '';
 }
+
+while [ -n "$1" ]; do
+	if [ "$1" = "-h" ];then
+		shift
+	elif [ "$1" = "-s" ]; then
+		shift
+	elif [ "$1" = "-d" ]; then
+		shift
+	elif [ "$1" = "-R" ]; then
+		shift
+	elif [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+		print_usage;
+		exit 0;
+	else
+		print_usage;
+		exit 1;
+	fi
+	shift
+done
+
