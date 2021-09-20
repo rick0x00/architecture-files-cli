fb6e7e26d0f1e57c0b2d6409e987eda1cab5a86c
silvajhb <jhbs.student@gmail.com>
2021-09-20 13:32:10 -0300

    adjust loop

--- mkarchdir.sh
+++ mkarchdir.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 exitcode='0';
-
+s='0';
 localdir=$(pwd);
 cd "$localdir";
 
 GitHubDirSource(){
 	echo "Project source: https://github.com/silvajhb/architecture-files-cli";
+}
+
+ExitFunction(){
+	exit $exitcode;
 }
 
 print_usage(){
@@ -26,13 +30,11 @@ CreateHWDir(){
 	if [ -d "$localdir/HARDWARE" ]; then
 		echo 'HARDWARE Directory is already exist!';			
 		exitcode='1';
-		exit $exitcode;
 	else
 		echo 'HARDWARE Directory not exist!';
 		echo 'Start create HARDWARE directory!';
 		mkdir -v "$localdir"/HARDWARE;
 		exitcode='0';
-		exit $exitcode;
 	fi
 }
 
@@ -40,37 +42,45 @@ CreateSWDir(){
 	if [ -d "$localdir/SOFTWARE" ]; then
 		echo 'SOFTWARE Directory is already exist!';			
 		exitcode='1';
-		exit $exitcode;
 	else
 		echo 'SOFTWARE Directory not exist!';
 		echo 'Start create SOFTWARE directory!';
 		mkdir -v "$localdir"/SOFTWARE;
 		exitcode='0';
-		exit $exitcode;
 	fi
 
 }
 
 while [ -n "$1" ]; do
 	if [ "$1" = "-hw" ]; then
-		shift
 		CreateHWDir;
-	elif [ "$1" = "-sw" ]; then
+		shift;
+		s='0';
+	fi
+	if [ "$1" = "-sw" ]; then
+		CreateSWDir;
+		shift;
+		s='0';
+	fi
+	if [ "$1" = "-docs" ]; then
 		shift
-		createSWDir;
-	elif [ "$1" = "-docs" ]; then
+		s='0';
+	fi
+	if [ "$1" = "-readme" ]; then
 		shift
-	elif [ "$1" = "-readme" ]; then
-		shift
-	elif [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
+		s='0';	
+	fi
+	if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
 		print_usage;
 		exitcode='0';
-		exit $exitcode;
+		s='0';
+		ExitFunction;
 	else
-		print_usage;
-		exitcode='1';
-		exit $exitcode;
+		s="$s"+1;
+		if [ "$s" = "2" ]; then
+			s='0';
+			ExitFunction;
+		fi
 	fi
-	shift
 done
 
