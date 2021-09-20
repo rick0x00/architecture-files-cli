4b2419d805656ff9002e11ca88252012ffbe6c50
silvajhb <jhbs.student@gmail.com>
2021-09-19 23:18:27 -0300

    add HW create files function, and adjust exitcode error

--- mkarchdir.sh
+++ mkarchdir.sh
@@ -1,39 +1,59 @@
 #!/bin/bash
 
+exitcode='0';
+
 localdir=$(pwd);
 cd "$localdir";
 
-GITHUBDIR(){
+GitHubDirSource(){
 	echo "Project source: https://github.com/silvajhb/architecture-files-cli";
 }
 
 print_usage(){
 	echo '';
-	GITHUBDIR;
-	echo 'Usage: mkarchdir.sh1 [ -h ] [ -s ] [ -d ] [ -R ]';
+	GitHubDirSource;
+	echo '';
+	echo 'Usage: mkarchdir.sh1 [ -hw ] [ -sw ] [ -docs ] [ -readme ]';
 	echo 'Option: (optional)';
-	echo '	-h	Add Hardware Directories';
-	echo '	-s	Add Software Directories';
-	echo '	-d	Add Docments Directories';
-	echo '	-R	Add README.md Files in all Directories(All README.md files recept respective title)';
+	echo '	-hw	Add Hardware Directories';
+	echo '	-sw	Add Software Directories';
+	echo '	-docs	Add Docments Directories';
+	echo '	-readme	Add README.md Files in all Directories(All README.md files recept respective title)';
 	echo '';
 }
 
+CreateHWDir(){
+	if [ -d "$localdir/HARDWARE" ]; then
+		echo 'Directory is already exist!';			
+		exitcode='1';
+		exit $exitcode;
+	else
+		echo 'Directory not exist!';
+		echo 'Start create directory!';
+		mkdir -v "$localdir"/HARDWARE;
+		exitcode='0';
+		exit $exitcode;
+	fi
+}
+
 while [ -n "$1" ]; do
-	if [ "$1" = "-h" ];then
+	if [ "$1" = "-hw" ]; then
 		shift
-	elif [ "$1" = "-s" ]; then
+		CreateHWDir;
+	elif [ "$1" = "-sw" ]; then
 		shift
-	elif [ "$1" = "-d" ]; then
+	elif [ "$1" = "-docs" ]; then
 		shift
-	elif [ "$1" = "-R" ]; then
+	elif [ "$1" = "-readme" ]; then
 		shift
 	elif [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
 		print_usage;
-		exit 0;
+		exitcode='0';
+		exit $exitcode;
 	else
 		print_usage;
-		exit 1;
+		exitcode='1';
+		exit $exitcode;
 	fi
 	shift
 done
